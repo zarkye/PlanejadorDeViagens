67735d9c2d263b375bd265f1638862fb567959c0
Lucas Fagundes <lucasfagundesice@gmail.com>
2025-08-07 21:57:39 -0300

    fazendo o controller2

--- src/main/java/br/cesul/planejadorviagens/MainApp.java
+++ src/main/java/br/cesul/planejadorviagens/MainApp.java
@@ -1,7 +1,27 @@
 package br.cesul.planejadorviagens;
 
-public class MainApp {
-    public static void main(String[] args) {
 
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+
+        var loader = new FXMLLoader(getClass().getResource("/br.cesul.planejadorviagens/trip_view.fxml"));
+
+        Parent root = loader.load();
+
+        stage.setTitle("Trip Planner");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/main/java/br/cesul/planejadorviagens/controller/TripController.java
+++ src/main/java/br/cesul/planejadorviagens/controller/TripController.java
@@ -1,6 +1,7 @@
 package br.cesul.planejadorviagens.controller;
 
 import br.cesul.planejadorviagens.model.Viagem;
+import br.cesul.planejadorviagens.services.PlanejamentoService;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import org.w3c.dom.Text;
@@ -14,9 +15,13 @@ public class TripController {
     @FXML private Text orcamentoField;
     @FXML private Button btnAdicionar;
     @FXML private TableView<Viagem> viagensTable;
-    @FXML private TableColumn<> colCidade;
-    @FXML private TableColumn<> colIni;
-    @FXML private TableColumn<> colFim;
-    @FXML private TableColumn<> colCusto;
+    @FXML private TableColumn<Viagem, String> colCidade;
+    @FXML private TableColumn<Viagem, String> colIni;
+    @FXML private TableColumn<Viagem, String> colFim;
+    @FXML private TableColumn<Viagem, Number> colCusto;
     @FXML private Label lblTotal;
+    private final PlanejamentoService service = new PlanejamentoService();
+    @FXML private void initialize(){
+        
+    }
 }
